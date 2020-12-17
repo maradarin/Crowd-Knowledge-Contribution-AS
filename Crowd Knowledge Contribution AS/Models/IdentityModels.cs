53099d82bb5a626e9f14fa4eadba52c7690f64d4
Luana <62220895+st-lu@users.noreply.github.com>
2020-12-17 23:47:09 +0200

    Admin can now edit user info and change roles

--- Crowd Knowledge Contribution AS/Models/IdentityModels.cs
+++ Crowd Knowledge Contribution AS/Models/IdentityModels.cs
@@ -1,6 +1,8 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Crowd_Knowledge_Contribution.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -10,6 +12,8 @@ namespace Crowd_Knowledge_Contribution_AS.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public IEnumerable<SelectListItem> AllRoles { get; internal set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
