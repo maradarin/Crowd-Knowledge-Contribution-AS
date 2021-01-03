82a8220320cd4dd90d5cbe5e7e3a0897d9a7a402
Agha Mara <62206596+maradarin@users.noreply.github.com>
2021-01-04 00:16:37 +0200

    pagina principala

--- Crowd Knowledge Contribution AS/Controllers/HomeController.cs
+++ Crowd Knowledge Contribution AS/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Crowd_Knowledge_Contribution.Models;
+using Crowd_Knowledge_Contribution_AS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,42 @@ namespace Crowd_Knowledge_Contribution_AS.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            List<Category> categories = db.Categories.ToList();
+            List<Article> articles = new List<Article>();
+            foreach (Category category in categories)
+            {
+                Article article = db.Articles.OrderByDescending(a => a.LastModified).First(a => a.CategoryId == category.CategoryId);
+                articles.Add(article);
+            }
+            ViewBag.Categories = categories;
+            ViewBag.Articles = articles;
+            return View();
+        }
+
+        public ActionResult Show(int id, string sortOrder)
+        {
+            List<Article> articles = db.Articles.Where(a => a.CategoryId == id).ToList();
+            Category category = db.Categories.First(c => c.CategoryId == id);
+            switch (sortOrder)
+            {
+                case "articleTitle":
+                    articles = db.Articles.Include("Category").Include("User").Where(a => a.CategoryId == id).OrderByDescending(a => a.ArticleTitle).ToList();
+                    break;
+                case "lastModified":
+                    articles = db.Articles.Include("Category").Include("User").Where(a => a.CategoryId == id).OrderByDescending(a => a.LastModified).ToList();
+                    break;
+                case "userName":
+                    articles = db.Articles.Include("Category").Include("User").Where(a => a.CategoryId == id).OrderByDescending(a => a.User.UserName).ToList();
+                    break;
+                default:
+                    articles = db.Articles.Include("Category").Include("User").Where(a => a.CategoryId == id).OrderByDescending(a => a.ArticleTitle).ToList();
+                    break;
+            }
+            ViewBag.Articles = articles;
+            ViewBag.Category = category;
             return View();
         }
 
